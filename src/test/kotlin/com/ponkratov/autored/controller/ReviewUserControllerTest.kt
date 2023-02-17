cad4b448d350728d92f5bf498eb8337aa9edbeb8
Alexey Ponkratov <super.ponkrat2014@yandex.ru>
2023-02-17 19:27:49 +0300

    Added Unit tests

--- src/test/kotlin/com/ponkratov/autored/controller/ReviewUserControllerTest.kt
+++ src/test/kotlin/com/ponkratov/autored/controller/ReviewUserControllerTest.kt
@@ -1,6 +1,7 @@
 package com.ponkratov.autored.controller
 
 import com.ponkratov.autored.AutoredApplication
+import com.ponkratov.autored.dto.response.MessageResponse
 import com.ponkratov.autored.model.ReviewUser
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
@@ -8,23 +9,27 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.exchange
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
 
 
-@SpringBootTest(classes = [AutoredApplication::class],
-    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(
+    classes = [AutoredApplication::class],
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 internal class ReviewUserControllerTest {
 
     @Autowired
     lateinit var restTemplate: TestRestTemplate
 
     @Test
-    fun addReview_returnsOkResponse1() {
-        val reviewUser = ReviewUser(mark = 5, comment = "Nice guy!", userFrom = 1, userTo = 7)
+    fun addReview_returnsOkResponse() {
+        val reviewUser = ReviewUser(mark = 5, comment = "Nice guy!", userFrom = 11, userTo = 15)
         val headers = HttpHeaders()
         headers.setBearerAuth("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwb25rcmF0b3Yud29ya0BnbWFpbC5jb20iLCJpYXQiOjE2NzY2NDU2NDAsImV4cCI6MTY3NjY1MzI0MH0.9hCIYQWb-oeuPHaRq6w7d6AKi3rFwiE8QKCVqVD1dDvgBfUJs16xnhqfru8LLNX-cu9aG6ZXUS8fF6Fp7fUTEQ")
 
@@ -33,13 +38,24 @@ internal class ReviewUserControllerTest {
 
         val entity: HttpEntity<MultiValueMap<String, Any>> = HttpEntity(body, headers)
         val result = restTemplate
-            .postForEntity("/api/review/user/add", entity, String::class.java)
+            .postForEntity("/api/review/user/add", entity, MessageResponse::class.java)
 
         assertNotNull(result)
         assertEquals(HttpStatus.OK, result?.statusCode)
+        assertEquals(result?.body?.message, "Review added successfully")
     }
 
     @Test
-    fun deleteReviewById() {
+    fun deleteReviewById_returnsOkResponse() {
+        val headers = HttpHeaders()
+        headers.setBearerAuth("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwb25rcmF0b3Yud29ya0BnbWFpbC5jb20iLCJpYXQiOjE2NzY2NDU2NDAsImV4cCI6MTY3NjY1MzI0MH0.9hCIYQWb-oeuPHaRq6w7d6AKi3rFwiE8QKCVqVD1dDvgBfUJs16xnhqfru8LLNX-cu9aG6ZXUS8fF6Fp7fUTEQ")
+
+        val entity: HttpEntity<MultiValueMap<String, Any>> = HttpEntity(headers)
+        val result =
+            restTemplate.exchange("/api/review/user/delete/13", HttpMethod.DELETE, entity, MessageResponse::class.java)
+
+        assertNotNull(result)
+        assertEquals(result?.body?.message, "Review deleted successfully")
+        assertEquals(HttpStatus.OK, result?.statusCode)
     }
 }
